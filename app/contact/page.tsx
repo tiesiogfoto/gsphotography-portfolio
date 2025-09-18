d1497a09df6575992a5e133757b7f5dc3979fff8
tiesiogfoto <tiesiogfoto@gmail.com>
2025-09-18 12:37:29 +0200

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,3 @@
-<p className="mt-4">
-  Telefon: <a href="tel:46262381" className="text-green-500">46 26 23 81</a>
-</p>
 <p>
   E-post: <a href="mailto:info@gsphotography.no" className="text-green-500">info@gsphotography.no</a>
 </p>
