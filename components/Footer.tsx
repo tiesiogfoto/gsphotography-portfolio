964550993d45e025b357a7dcc7c9fec08a8f205b
tiesiogfoto <tiesiogfoto@gmail.com>
2025-09-18 10:34:25 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,9 @@ export default function Footer() {
   return (
     <footer className="p-6 border-t mt-12 text-sm text-gray-500">
       <div className="max-w-6xl mx-auto">
-        © {new Date().getFullYear()} GSphotography. All rights reserved.
+        © {new Date().getFullYear()} GSphotography. Alle rettigheter reservert.
       </div>
     </footer>
   );
 }
+
