74259dc11e688f369b7bea359fbb6e71d9e110c5
tiesiogfoto <tiesiogfoto@gmail.com>
2025-09-18 10:36:26 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,8 @@
 export default function Hero() {
   return (
     <section className="max-w-6xl mx-auto py-16 px-4">
-      <h1 className="text-3xl font-bold mb-2">Clean, timeless photography</h1>
-      <p>for brands and people.</p>
+      <h1 className="text-3xl font-bold mb-2">Ren og tidløs fotografering</h1>
+      <p>for merkevarer og mennesker.</p>
     </section>
   );
 }
